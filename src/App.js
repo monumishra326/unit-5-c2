153968dd96de404f320c8c018de8dab3f5374359
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-24 17:50:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import {useState} from 'react';
-// import './App.css';
-import { AddHouse } from './components/AddHouse/AddHouse';
-import { Rentals } from './components/Rentals/Rentals';
+// import "./App.css";
+import { Rentals } from "./Components/Rentals/Rentals";
+import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
 
 function App() {
-  const [show,setShow] = useState(false)
+  const [show, setShow] = useState(false);
   return (
     <div className="App">
-      
-      <button className="toggleForm" onClick={()=>{
-        show?setShow(false):setShow(true)
-      }}>
-        {show?"Show Rentals":"Add House"}
+      <button
+        className="toggleForm"
+        onClick={() => {
+          show ? setShow(false) : setShow(true);
+        }}
+      >
+        {show ? "Show Rentals" : "Add House"}
       </button>
-      {show?<AddHouse/>:<Rentals/>}
+      {show ? <AddHouse /> : <Rentals />}
       <br />
     </div>
   );
