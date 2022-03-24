9123bd55d2435bf229fa1dfb02636670d8740228
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-24 17:48:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import './App.css';
+// import './App.css';
 import { AddHouse } from './components/AddHouse/AddHouse';
 import { Rentals } from './components/Rentals/Rentals';
 
